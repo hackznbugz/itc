e3f72201c727d62d365677919f6ebb900075e9b0
Akbar Ali <51843650+hackznbugz@users.noreply.github.com>
2021-09-28 13:04:51 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
 
 // gallery
+
+var images = [
+    'gallery/1.png',
+    'gallery/2.png',
+    'gallery/3.png',
+    'gallery/4.png',
+    'gallery/5.png',
+    'gallery/6.png',
+    'gallery/7.png',
+    'gallery/8.png',
+]
+
 $('.gallery .image').each(function(index){
     $(this).css({'background':'url(gallery/' + (index + 1) +'.png)', 'background-size': 'cover'});
+});
+
+$('.gallery .image').click(function(){
+    var bg = $(this).css('background').replace(/^url\(['"](.+)['"]\)/, '$1');
+    $('.gallery-carousel').css({'display':'block'});
+    $('.gallery-carousel .image').css({'background':bg});
+    index = $(this).index() + 1;
+});
+
+$('.gallery-carousel .arrow.right').click(function(){
+    index = index + 1;
+    if(index > images.length){
+        index = 1;
+    }
+    $('.gallery-carousel .image').css({'background':'url(' + images[index-1] + ')'});
+});
+
+$('.gallery-carousel .arrow.left').click(function(){
+    index = index - 1;
+    if(index == 0){
+        index = images.length;
+    }
+    $('.gallery-carousel .image').css({'background':'url(' + images[index-1] + ')'});
+});
+
+$('.gallery-carousel .top i').click(function(){
+    $('.gallery-carousel').css({'display':'none'});
 });
 
 
