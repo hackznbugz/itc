f21f25d4ae5c9bd256f191109e781d153b37d399
pulimoodan <akbaraliongo@gmail.com>
2024-01-22 16:10:19 +0530

    updates nav

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,22 @@
- // nav menu
+// nav menu
 $('.hamburger').click(function () {
-    $('.nav-box').css({'transform': 'translateX(0)'});
-});
-$('.nav-box-close').click(function () {
-    $('.nav-box').css({'transform': 'translateX(100%)'});
-});
+    if ($(this).hasClass('open')) {
+        $(this).removeClass('open')
+        $('.links').css({ transform: 'translateX(100%)' })
+    } else {
+        $(this).addClass('open')
+        $('.links').css({ transform: 'translateX(0)' })
+    }
+})
 
- 
- // sticky navbar
- window.onscroll = function() {
-
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop + 100;
+// sticky navbar
+window.onscroll = function () {
+    var navbar = document.getElementById('navbar')
+    var sticky = navbar.offsetTop + 100
 
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
+        navbar.classList.add('sticky')
     } else {
-        navbar.classList.remove("sticky");
+        navbar.classList.remove('sticky')
     }
-};
+}
